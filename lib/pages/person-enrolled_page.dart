a4c098388ac305465d6e52db96eefb6dafb33152
flo <fw-224686@hs-weingarten.de>
2024-04-29 18:01:21 +0200

    basic arcitrcture principle

---
+++ lib/pages/person-enrolled_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class PersonEnrolledPage extends StatelessWidget {
+  @override
+  final Key? key;
+
+  const PersonEnrolledPage({this.key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text(
+          'Content of the page',
+          style: TextStyle(fontSize: 20.0),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_add),
+            label: 'Register',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.folder),
+            label: 'Case',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.forum),
+            label: 'Forum',
+          ),
+        ],
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+      ),
+    );
+  }
+}
