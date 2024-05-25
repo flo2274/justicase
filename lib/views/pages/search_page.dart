245d19cea29f34625da9224c71aff79cf04b345a
Florian <fw-224686@hs-weingarten.de>
2024-05-25 21:42:10 +0200

    Implement Search Bar with autocompletion

--- lib/views/pages/search_page.dart
+++ lib/views/pages/search_page.dart
@@ -1,5 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:go_router/go_router.dart';
+
+class Company {
+  final String name;
+
+  Company({required this.name});
+}
+
+class CompanyPage extends StatelessWidget {
+  final Company company;
+
+  const CompanyPage({required this.company});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(company.name),
+      ),
+      body: Center(
+        child: Text('Details about ${company.name}'),
+      ),
+    );
+  }
+}
+
+class CompanyService {
+  static List<Company> find(String search) {
+    // Simulated search function
+    return _companies
+        .where((company) => company.name.toLowerCase().contains(search.toLowerCase()))
+        .toList();
+  }
+
+  static final List<Company> _companies = [
+    Company(name: 'Apple'),
+    Company(name: 'Google'),
+    Company(name: 'Microsoft'),
+    Company(name: 'Amazon'),
+    Company(name: 'Facebook'),
+    Company(name: 'Tesla'),
+    Company(name: 'Twitter'),
+    Company(name: 'Netflix'),
+    Company(name: 'Disney'),
+    Company(name: 'Intel'),
+  ];
+}
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -9,22 +56,65 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  final TextEditingController _controller = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _focusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Center(child: Text('JUSTICASE')),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text('Screen',
-                style: Theme.of(context).textTheme.titleLarge),
-            const Padding(padding: EdgeInsets.all(4)),
-            TextButton(
-              onPressed: () => context.go('/case/grouping'), // /case/grouping oder /search/grouping
-              child: const Text('View details'),
+          children: [
+            TypeAheadField<Company>(
+              suggestionsCallback: (search) => CompanyService.find(search),
+              itemBuilder: (context, Company suggestion) {
+                return ListTile(
+                  title: Text(suggestion.name),
+                );
+              },
+              onSelected: (Company suggestion) {
+                Navigator.of(context).push<void>(
+                  MaterialPageRoute(
+                    builder: (context) => CompanyPage(company: suggestion),
+                  ),
+                );
+              },
+              builder: (context, controller, focusNode) {
+                return TextField(
+                  controller: controller,
+                  focusNode: focusNode,
+                  autofocus: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Search...',
+                  ),
+                );
+              },
+            ),
+            Center(//Todo: Remove because only temporary to show routing
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text('Screen',
+                      style: Theme.of(context).textTheme.titleLarge),
+                  const Padding(padding: EdgeInsets.all(4)),
+                  TextButton(
+                    onPressed: () => context.go('/case/grouping'), // /case/grouping oder /search/grouping
+                    child: const Text('View details'),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
